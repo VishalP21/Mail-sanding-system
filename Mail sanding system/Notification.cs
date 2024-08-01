9c38fb550bebf226ba49f353e56f015eaaea47ff
Vishal Prajapati <vishalp951295@gmail.com>
2024-08-01 08:53:27 +0530

    Add project files.

---
+++ Mail sanding system/Notification.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace Mail_sanding_system
+{
+    public class Notification
+    {
+        public void sandemailNotification(string to, string subject, string body)
+        {
+            var too =new MailAddress(to);
+            var from = new MailAddress("vishalp951295@gmail.com");
+            var password = "aoll ijcd ityg matl";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(from.Address, password)
+
+            };
+
+            var v = new MailMessage(from, too);
+            
+                v.Subject = subject;
+            v.Body = body;
+            smtp.Send(v);   
+        }
+    }
+}
---
+++ Mail sanding system/Program.cs
@@ -0,0 +1,8 @@
+﻿using Mail_sanding_system;
+
+Notification notification=new Notification();
+
+for (int i = 0; i < 3; i++)
+{
+    notification.sandemailNotification("vishalp951295@gmail.com", "jamvanu thi gau 6", "vela te pela navi sysytem 6");
+}
